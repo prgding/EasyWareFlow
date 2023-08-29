b85e70e3784fe784bee7530e7a2bda9aa4051025
ding <69354087+prgding@users.noreply.github.com>
2023-08-29 10:33:23 +0800

    Init

---
+++ src/main/java/me/ding/easywareflow/EasyWareFlowApplication.java
@@ -0,0 +1,13 @@
+package me.ding.easywareflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyWareFlowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyWareFlowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/ding/easywareflow/EasyWareFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package me.ding.easywareflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyWareFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
